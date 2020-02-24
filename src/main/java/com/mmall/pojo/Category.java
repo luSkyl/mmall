40e5dda185da3126760ad3fef2151d2d5070e38f
luSkyl <1527531356@qq.com>
2020-02-24 08:16:11 +0800

    project init commit

---
+++ src/main/java/com/mmall/pojo/Category.java
@@ -0,0 +1,27 @@
+package com.mmall.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    private Integer id;
+
+    private Integer parentId;
+
+    private String name;
+
+    private Boolean status;
+
+    private Integer sortOrder;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+}
